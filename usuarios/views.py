959ecd60609649b97504b6a51356b9eed3119994
Philip Martins <philip.martt@outlook.com>
2025-08-08 15:01:10 -0300

    feat: login

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib import auth
 
 
 def cadastro(request):
@@ -31,3 +33,20 @@ def cadastro(request):
         )
 
         return redirect('/usuarios/login')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('treinar_ia')
+        
+        messages.add_message(request, constants.ERROR, 'Username ou senha inválidos.')
+        return redirect('login')
